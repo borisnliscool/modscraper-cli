86cd232869d4e8f1c41f612520ad51e866a00a50
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-09-03 15:08:43 +0200

    make it work as cli

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
+#!/usr/bin/env node
+
 import fs from "fs";
 import path from "path";
 import { downloadMod } from "./src/download.js";
 import { log } from "./src/logger.js";
 import chalk from "chalk";
+import { program } from "commander";
 
-const url = process.argv[2];
+async function handleAction(url) {
+	const options = program.opts();
 
-if (!url) {
-	log(
-		chalk.red(
-			"Please provide a Modscraper-web share link as a command-line argument."
-		)
+	const [buffer, ext] = await downloadMod(url);
+	const archivePath = path.join(
+		process.cwd(),
+		(options.output ? options.output : "msw-download") + ext
 	);
-	log(
-		chalk.red(
-			`You can get one by clicking 'Copy share link' on any mod on ${chalk.underline(
-				"https://msw.boris.foo/search"
-			)}`
-		)
-	);
-	process.exit(1);
+	log("Writing archive..");
+	fs.writeFileSync(archivePath, buffer);
+
+	log(`Downloaded to "${chalk.green(archivePath)}"`);
 }
 
-const [buffer, ext] = await downloadMod(url);
-const archivePath = path.join(process.cwd(), "msw-download" + ext);
-
-log("Writing archive..");
-fs.writeFileSync(archivePath, buffer);
-
-log(`Downloaded to "${chalk.green(archivePath)}"`);
+program
+	.name("msw")
+	.arguments("<url>", "msw share link to the mod")
+	.option("-o, --output <name>", "the name of the output archive")
+	.action(handleAction)
+	.parse(process.argv);
